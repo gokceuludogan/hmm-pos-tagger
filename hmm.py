80dd057c0ff3f142de31cd960847b584d526b712
gokce <gokceuludogan@gmail.com>
2018-11-16 23:56:46 +0300

    convert hmm to class and add confusion matrix plot

--- hmm.py
+++ hmm.py
@@ -2,12 +2,110 @@
 from random import shuffle
 from random import seed
 import numpy as np
+import seaborn as sns; sns.set()
+from matplotlib.colors import LogNorm
+import matplotlib.pyplot as plt
+import itertools
+class HMM:
+
+    def __init__(self, train_data, test_data):
+        self.train_data = train_data
+        self.test_data = test_data
+        self.tags = []
+        self.words = []
+        self.tag_dict = {}
+        self.word_dict = {}
+        self.num_of_tags = 0
+        self.transition_probs = None
+        self.emission_probs = None
+        self.initial_probs = None
+
+    def train(self):
+        self.tags = list(set([tag for sequence in train_data for word, tag in sequence]))
+        self.tag_dict = enumerate_list(self.tags)    
+        self.words = list(set([word.lower() for sequence in train_data for word, tag in sequence]))
+        self.word_dict = enumerate_list(self.words)
+        self.num_of_tags = len(self.tags)
+        transition_probs= np.zeros((self.num_of_tags, self.num_of_tags)) 
+        emission_probs = np.zeros((self.num_of_tags, len(self.words)))
+        initial_probs = np.zeros(self.num_of_tags)
+        for sequence in self.train_data:
+            prev_tag = 'None'
+            for word, tag in sequence:
+                word_lower = word.lower()
+                tag_id = self.tag_dict[tag]
+                word_id = self.word_dict[word_lower]
+                if prev_tag != 'None':
+                    transition_probs[self.tag_dict[prev_tag], tag_id] += 1  
+                    prev_tag = tag
+                else:
+                    prev_tag = tag
+                    initial_probs[tag_id] += 1    
+                if word_id not in emission_probs[tag_id]:
+                    emission_probs[tag_id, word_id] = 1
+                else:
+                    emission_probs[tag_id, word_id] += 1
+        self.transition_probs = normalize(transition_probs)
+        self.emission_probs = normalize(emission_probs)
+        self.initial_probs = initial_probs/initial_probs.sum()           
+
+    def test(self):
+        sentence_truth = [0, 0]
+        word_truth = [0, 0]
+        conf_mat = np.zeros((self.num_of_tags, self.num_of_tags))
+        for sequence in self.test_data:
+            actual_tags = list(map(lambda x: self.tag_dict[x[1]], sequence))
+            predicted_tags = self.viterbi(list(map(lambda x: self.word_dict.get(x[0].lower(), -1), sequence)))
+            print(' '.join(map(lambda x: x[0], sequence)))
+            print(' '.join([str(tag) for tag in predicted_tags]))
+            #sentence_truth[actual_tags == predicted_tags] += 1
+            if actual_tags == map(lambda x: self.tags[x], predicted_tags):
+                sentence_truth[0] += 1
+            else: 
+                sentence_truth[1] += 1
+            for actual_tag, predicted_tag in zip(actual_tags, predicted_tags):
+                #word_truth[actual_tag == predicted_tag] += 1
+                if actual_tag == predicted_tag:
+                    print('correct')
+                    word_truth[0] += 1
+                else: 
+                    print('wrong')
+                    word_truth[1] += 1
+                conf_mat[actual_tag, predicted_tag] += 1  
+        print(self.tag_dict)
+        print(sentence_truth)
+        print(word_truth)
+        plot_confusion_matrix(conf_mat, normalize=False)
+        #ax = sns.heatmap(conf_mat, cmap='viridis')
+        #ax.set_title('Confusion Matrix')
+        #ax.figure.savefig("output.png")
+        return word_truth, sentence_truth, conf_mat
+
+    def viterbi(self, sequence):
+        seq_len = len(sequence)
+        V = np.zeros((self.num_of_tags, seq_len))
+        B = np.zeros((self.num_of_tags, seq_len))    
+        V[:, 0] = self.initial_probs * self.emission_probs[:, sequence[0]]
+        for t in range(1, seq_len):
+            for s in range(self.num_of_tags):
+                result = V[:, t-1] * self.transition_probs[:, s] * self.emission_probs[s, sequence[t]]
+                V[s, t] = max(result)
+                B[s, t] = np.argmax(result)
+       
+        V[:, seq_len-1] = max(V[:, seq_len-1])  
+        B[:, seq_len-1] = np.argmax(V[:, seq_len-1])    
+
+        x = np.empty(seq_len, 'B')
+        x[-1] = np.argmax(V[:, seq_len - 1])
+        for i in reversed(range(1, seq_len)):
+            x[i - 1] = B[x[i], i]
+        return x
+
+
 def get_data(filepath):
-    raw_data = open(filepath,encoding='utf8').readlines()
+    raw_data = open(filepath, encoding='utf8').readlines()
     all_sequences = []
     current_sequence = []
-    all_tags = []
-    #all_words = []
     for instance in raw_data:
         if instance != '\n':
             cols = instance.split()
@@ -16,147 +114,62 @@ def get_data(filepath):
                     current_sequence.append((cols[1], 'Noun'))
                 else:    
                     current_sequence.append((cols[1], cols[3]))
-                    if cols[3] not in all_tags:
-                        all_tags.append(cols[3])
-                    # if cols[1].lower() not in all_words:
-                    #   all_words.append(cols[1])   
         else:
             all_sequences.append(current_sequence)
             current_sequence = []   
-    print('All tags')
-    print(','.join(all_tags))
-    return all_sequences, all_tags
+    return all_sequences
 
 def split_data(data, percent):
     shuffle(data)
     train_size = int(len(data) * percent / 100)
     return data[:train_size], data[train_size:]
 
-def training(train_data, all_tags):
-    tag_dict = enumerate_list(all_tags)
-    distinct_words = set([word.lower() for sequence in train_data for word, tag in sequence])
-    word_dict = enumerate_list(list(distinct_words))
-    transition_matrix = np.zeros((len(tag_dict), len(tag_dict))) 
-    #[[0 for i in range(len(tag_dict))] for j in range(len(tag_dict))]
-    observations = np.zeros((len(tag_dict), len(word_dict)))
-    #[[0 for j in range(len(word_dict))] for i in range(len(tag_dict))]
-    initial_probability = np.zeros(len(tag_dict))
-    #[0 for i in range(len(tag_dict))]
-    for sequence in train_data:
-        prev_tag = 'None'
-        for word, tag in sequence:
-            word_lower = word.lower()
-            tag_id = tag_dict[tag]
-            word_id = word_dict[word_lower]
-            if prev_tag != 'None':
-                transition_matrix[tag_dict[prev_tag], tag_id] += 1  
-                prev_tag = tag
-            else:
-                prev_tag = tag
-                initial_probability[tag_id] += 1    
-            if word_id not in observations[tag_id]:
-                observations[tag_id, word_id] = 1
-            else:
-                observations[tag_id, word_id] += 1
-    return normalize(transition_matrix), initial_probability/initial_probability.sum(), normalize(observations), tag_dict, word_dict, distinct_words, all_tags
-
-
-
 def normalize(matrix):
     row_sums = matrix.sum(axis=1)
-    return matrix / row_sums[:, np.newaxis]
-
-
-def test(data, tag_dict, word_dict, distinct_words, distinct_tags):
-    correct_sentence = 0
-    wrong_sentence = 0
-    correct_word = 0
-    wrong_word = 0
-    conf_mat = np.zeros((len(tag_dict), len(tag_dict)))
-    [[0 for i in range(len(tag_dict))] for j in range(len(tag_dict)) ]
-    for sequence in data:
-        #print(sequence)
-        actual_tags = list(map(lambda x: x[1], sequence))
-        predicted_tags = viterbi(initial_prob, list(map(lambda x: word_dict.get(x[0].lower(), -1), sequence)), transition_matrix, obs)
-        #print(' '.join(actual_tags))
-        print(' '.join(map(lambda x: x[0], sequence)))
-        print(' '.join([distinct_tags[tag] for tag in predicted_tags]))
-        print(' '.join(actual_tags))
-        if actual_tags == map(lambda x: distinct_tags[x], predicted_tags):
-            correct_sentence += 1
-        else: 
-            wrong_sentence += 1
-        for x in range(len(predicted_tags)):
-            print(actual_tags[x])
-            print(tag_dict[actual_tags[x]] )
-            if tag_dict[actual_tags[x]] == predicted_tags[x]:
-                print('correct')
-                correct_word += 1
-            else: 
-                print('wrong')
-                wrong_word += 1
-            conf_mat[tag_dict[actual_tags[x]], predicted_tags[x]] += 1  
-        # for actual_tag, predicted_tag in zip(actual_tags, predicted_tags):
-        #     print('asdflksjfsd')
-        #     if tag_dict[actual_tag] == predicted_tag:
-        #         print('correct')
-        #         correct_word += 1
-        #     else: 
-        #         print('wrong')
-        #         wrong_word += 1
-        #         conf_mat[tag_dict[actual_tag]][predicted_tag] += 1  
-    print(tag_dict)
-    print(correct_word )
-    print(wrong_word)
-    print(conf_mat)
-            
+    return matrix / row_sums[:, np.newaxis]            
 
 def enumerate_list(data):
     return {instance: index for index, instance in enumerate(data)}
 
-# observations
-# states
-# p_init initial probabilities
-# sequence observations in time
-# Tr transitions
-# p_obs probability of seeing observation on a state
+def plot_confusion_matrix(cm, cmap=None, normalize = True, target_names = None, title = "Confusion Matrix"):
+    
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
 
-def viterbi(pi, sequence, transition_matrix, emission_prob):
-    num_of_states = transition_matrix.shape[0]
-    seq_len = len(sequence)
-    V = np.zeros((num_of_states, seq_len))
-    B = np.zeros((num_of_states, seq_len))    
-    V[:, 0] = pi * emission_prob[:, sequence[0]]
-    # print('initial')
-    # print(V[:, 0])
-    for t in range(1, seq_len):
-        for s in range(num_of_states):
-            # print('a')
-            # print(V[:, t-1])
-            # print(transition_matrix[:, s])
-            # print(emission_prob[s, sequence[t]])
-            result = V[:, t-1] * transition_matrix[:, s] * emission_prob[s, sequence[t]]
-            # print('seq ' + str(t))
-            # print('state ' + str(s))
-            # print(result)
-            V[s, t] = max(result)
-            B[s, t] = np.argmax(result)
-   
-    V[:, seq_len-1] = max(V[:, seq_len-1])  
-    B[:, seq_len-1] = np.argmax(V[:, seq_len-1])    
+    plt.figure(figsize=(24, 18))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    #plt.title(title)
+    plt.colorbar()
 
-    x = np.empty(seq_len, 'B')
-    x[-1] = np.argmax(V[:, seq_len - 1])
-    for i in reversed(range(1, seq_len)):
-        x[i - 1] = B[x[i], i]
-    return x 
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    #plt.ylabel('True label')
+    #plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.savefig('output.png')
+
 seed(5)
-all_sequences, all_tags = get_data('../Project (Application 1) (MetuSabanci Treebank).conll')
+all_sequences = get_data('Project (Application 1) (MetuSabanci Treebank).conll')
 train_data, test_data = split_data(all_sequences, 90)
-
-transition_matrix, initial_prob, obs, tag_dict, word_dict, distinct_words, distinct_tags = training(train_data, all_tags)
-#print(obs)
-test(test_data, tag_dict, word_dict, distinct_words, distinct_tags)
-#print(transition_matrix)
-#print(initial_prob)
-#print(obs)
+hmm = HMM(train_data, test_data)
+hmm.train()
+hmm.test()
